06069186dca4e0bdb6856ed3a0a540ea452f9dd5
Er1ck54 <erickmontap544@gmail.com>
2023-02-23 00:48:40 -0600

    Avances

---
+++ Proyecto-uni-Programacion-main/Avances/Boceto_Materias.js
@@ -0,0 +1,14 @@
+class Materias{
+    Materias;
+    #Carrera;
+    constructor(materias,carrera){
+
+        this.Materias=materias;
+        this.Carrera=carrera;
+    }
+
+}
+asignarcarrera=function (carrera){
+    this.carrera = function (carrera) ;
+
+}
